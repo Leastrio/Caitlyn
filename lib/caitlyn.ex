09f9969f77ee52d2ab3beae6efcc5bd526a56310
Leastrio <jacob@leastr.io>
2022-03-30 14:31:40 -0600

    Create base code

--- lib/caitlyn.ex
+++ lib/caitlyn.ex
@@ -1,18 +1,27 @@
 defmodule Caitlyn do
   @moduledoc """
-  Documentation for `Caitlyn`.
+  `Caitlyn` is a League of Legends API wrapper.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Caitlyn.hello()
-      :world
-
+  Initialize the api wrapper
   """
-  def hello do
+  def create do
+    api_key = Application.fetch_env!(:caitlyn, :api_key)
+    Agent.start_link(fn -> api_key end, name: __MODULE__)
+  end
 
+  @doc """
+  Initialize the api wrapper
+  """
+  def create(api_key) do
+    Agent.start_link(fn -> api_key end, name: __MODULE__)
+  end
+
+  @doc """
+  Get your api key
+  """
+  def key do
+    Agent.get(__MODULE__, fn state -> state end)
   end
 end
